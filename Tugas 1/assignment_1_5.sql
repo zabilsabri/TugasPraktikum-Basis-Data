78b71f0a9567002e972206b843cde06661fd32b5
zabil <zabilsabrimuhammad0@gmail.com>
2022-09-26 08:20:43 +0800

    Folder Management Fixed

---
+++ Tugas 1/assignment_1_5.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE db_praktikum;
+
+USE db_praktikum;
+
+CREATE TABLE buku(
+	judul VARCHAR (255),
+	`tahun terbit` INT,
+	pengarang VARCHAR (255),
+	id_buku INT AUTO_INCREMENT,
+	PRIMARY KEY (id_buku)
+);
+
+DESC buku;
+
+CREATE TABLE mahasiswa(
+	nama VARCHAR (255),
+	nim VARCHAR (10) NOT NULL UNIQUE,
+	jk CHAR (1),
+	id_mahasiswa BIGINT UNSIGNED AUTO_INCREMENT,
+	PRIMARY KEY (id_mahasiswa)
+);
+
+DESC mahasiswa;
+
+CREATE TABLE pinjam(
+	tgl_pinjam DATETIME,
+	id_mahasiswa BIGINT UNSIGNED,
+	id_buku INT,
+	status_pengembalian TINYINT (1) NOT NULL DEFAULT 0,
+	id_pinjam INT UNSIGNED AUTO_INCREMENT,
+	PRIMARY KEY (id_pinjam),
+	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa(id_mahasiswa),
+	FOREIGN KEY (id_buku) REFERENCES buku(id_buku)
+);
+
+DESC pinjam;
